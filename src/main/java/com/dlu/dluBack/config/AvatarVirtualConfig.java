3c61eae7a3acc486a7e5604eca9069f669c71dc0
adam <779093440@qq.com>
2023-05-11 16:06:12 +0800

    change：img path

--- src/main/java/com/dlu/dluBack/config/AvatarVirtualConfig.java
+++ src/main/java/com/dlu/dluBack/config/AvatarVirtualConfig.java
@@ -14,6 +14,6 @@ public class AvatarVirtualConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")//前端url访问的路径，若有访问前缀，在访问时添加即可，这里不需添加。
-                .addResourceLocations("file:E:/java_project/dlu_all/dluBack/src/main/java/com/dlu/dluBack/ftpfile/static/datasets/");//映射的服务器存放图片目录。
+                .addResourceLocations("file:E:/java_project/dlu_all/dluBack/ftpfile/static/datasets/");//映射的服务器存放图片目录。
     }
 }
